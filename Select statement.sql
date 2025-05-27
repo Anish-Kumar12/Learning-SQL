a274d65e487baa94e5e73c114733e96db705fecc
Anish Kumar <anishkumar344567@gmail.com>
2025-05-27 12:28:34 +0530

    Added Select statement.sql

---
+++ Select statement.sql
@@ -0,0 +1,5 @@
+USE sakila;
+#SELECT * FROM actor;
+#PEMDAS   it is the order of execution for operations in sql--
+#SELECT  first_name , last_name FROM actor;
+SELECT  DISTINCT first_name , last_name FROM actor;
