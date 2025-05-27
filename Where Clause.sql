c5f032eecf70c55649e57dbf46756cc3be7db7b5
Anish Kumar <anishkumar344567@gmail.com>
2025-05-27 13:16:02 +0530

    Added where Clause.sql

---
+++ Where Clause.sql
@@ -0,0 +1,22 @@
+USE sakila;
+-- SELECT * FROM actor;
+-- SELECT * FROM actor
+-- WHERE first_name = 'ED';
+SELECT * FROM payment
+WHERE amount = 5.99;
+
+
+SELECT * FROM payment
+WHERE amount <= 4;
+
+SELECT * FROM payment
+WHERE amount >= 4;
+
+SELECT * FROM payment
+WHERE amount != 5.99;
+
+
+
+
+
+
